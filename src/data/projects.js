6ce106387dd794f44baa0094cb2e96a7c99a1ad3
uniwaydev <uno.iwayuki.0911@outlook.com>
2024-05-30 07:02:30 -0700

    init

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+// Import images
+import MobileImage1 from '../images/mobile/1.png';
+import MobileImage2 from '../images/mobile/2.png';
+import MobileImage3 from '../images/mobile/3.png';
+import MobileImage4 from '../images/mobile/4.png';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Movie Hunt',
+		category: 'Android',
+		img: MobileImage1,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'Furniture Store',
+		category: 'IOS',
+		img: MobileImage2,
+	},
+	{
+		id: 3,
+		title: 'JetSunGlass',
+		category: 'Flutter',
+		img: MobileImage3,
+	},
+	{
+		id: 4,
+		title: 'CatTuong',
+		category: 'React Native',
+		img: MobileImage4,
+	},
+];
