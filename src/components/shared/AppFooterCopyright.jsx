c81a2aaf1d2a82b0ca539568588c8a3a082bc0e4
uniwaydev <uno.iwayuki.0911@outlook.com>
2024-05-31 06:47:49 -0700

    update website link

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -12,7 +12,7 @@ function AppFooterCopyright() {
 				</a>
 				@
 				<a
-					href="https://uno-iwayuki.vercel.app"
+					href="https://uno-fun.vercel.app"
 					target="__blank"
 					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
